1849bae765f3148284ff0354ff831be1b076bca7
jobdesc.kholifan <jobdesc.kholifanalfon@gmail.com>
2021-07-16 18:44:03 +0700

    ## 1.0.3
    * Form validate

--- lib/src/bs_inputtext.dart
+++ lib/src/bs_inputtext.dart
@@ -271,6 +271,11 @@ class _BsInputState extends State<BsInput> with SingleTickerProviderStateMixin {
                     )
                   ];
 
+                Future.delayed(Duration(milliseconds: 100), () {
+                  if(field.mounted)
+                    field.didChange(controller.text);
+                });
+
                 return Column(
                   children: [
                     Container(
